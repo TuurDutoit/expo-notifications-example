854c91dbda7e7b183348b95a25143cda7bef5aa7
Tuur Dutoit <tuur@datacamp.com>
2025-01-28 13:31:59 +0100

    chore: Log notification responses

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  React.useEffect(() => {
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log('NOTIFICATION RESPONSE RECEIVED', JSON.stringify(response, null, 2));
+    });
+    return () => subscription.remove();
+  }, []);
+
+  React.useEffect(() => {
+    async function logLastNotificationResponse() {
+      const response = await Notifications.getLastNotificationResponseAsync()
+      console.log('LAST NOTIFICATION RESPONSE', JSON.stringify(response, null, 2));
+    }
+    logLastNotificationResponse()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
